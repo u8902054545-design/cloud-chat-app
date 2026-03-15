0e8711ec86ccea63dd48f30e81a62915acde7f9b
u8902054545-design <u8902054545@gmail.com>
2026-03-15 12:34:58 +0300

    Fix relative paths and style imports

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '', // Это заставит пути быть относительными (./), что важно для APK
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 })
